7136cd221632e0369eaf1dff138252d6bbf9c72b
shivrajsingh123 <45819719+shivrajsingh123@users.noreply.github.com>
2025-03-11 11:49:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const url = window.location.href;
+
+// Get the length of the URL
+const urlLength = url.length;
+
+// Display the length using alert
+alert(`The length of the URL is: ${urlLength}`);
